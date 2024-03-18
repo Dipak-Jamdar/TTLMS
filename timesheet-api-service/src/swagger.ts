3bdc0ff5f2182effe219cfaffb43109a98ad6741
Dipak-Jamdar <dipjamdar@gmail.com>
2024-03-18 18:29:31 +0530

    adding files

---
+++ timesheet-api-service/src/swagger.ts
@@ -0,0 +1,15 @@
+// swagger.ts
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+export function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+}
